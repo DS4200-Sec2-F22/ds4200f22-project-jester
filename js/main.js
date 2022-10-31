f51e780b142d18af65ac94db9be7fc52f83b0855
Jeffrey Wallace <jcwallace002@gmail.com>
2022-10-30 21:33:19 -0400

    vSetup

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+// ----------CONSTANTS FOR PAGE SETUP----------------
+const FRAME_HEIGHT = 500;
+const FRAME_WIDTH = 500; 
+const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
+
+const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.left - MARGINS.right;
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.top - MARGINS.bottom; 
+
+// ----------SETTING THE FRAME FOR BOTH VISUALIZATIONS----------------
+
+
+// ---------FUNCTION TO BUILD BOTH PLOTS-------------
+function build_plots() {
+
+  // reading in data 
+  d3.csv("_____").then((data) => {
+
+    // -----------------PLOT 1-----------------
+    
+    // -----------------PLOT 2----------------
+   
+    });
+};
+
+build_plots();
+
+
+
+
+
+

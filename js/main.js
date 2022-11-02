5fd90139031e9bb5ca10b4e562a2f9f4f1194845
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-11-02 12:05:22 -0400

    completed

--- js/main.js
+++ js/main.js
@@ -127,16 +127,18 @@ function node_hover_out(event, d) {
 }
 // -----------------PLOT 2----------------
 
+
+
+
+
 function submitClicked() {
   const songTitle = document.getElementById('information').value; // gets the information from the textbox
   document.getElementById('information').value = ""; // sets textbox to "" 
   
-  
-  console.log(findInformationWithSong(songTitle))
   const id = findInformationWithSong(songTitle)
-
-  draw(id);
- 
+  
+  draw(id, svg);
+  
 }
 
 function findInformationWithSong(songTitle) {
@@ -148,7 +150,18 @@ function findInformationWithSong(songTitle) {
   }
 }
 
+
+
+const svg = d3.select("#col1")
+  .append("svg")
+  .attr("width", 650)
+  .attr("height", 650);
+
+
 function draw(id) {
+  
+  svg.selectAll("*").remove();
+
   const acoustincness = id.acousticness;
   const danceability = id.danceability;
   const energy = id.energy;
@@ -156,16 +169,16 @@ function draw(id) {
   const liveness = id.liveness;
   const speechiness = id.speechiness;
   const valence = id.valence;
-
+  
   // removed tempo becasue it's not from 0 - 1 
-
+  
   let features = ["Acousticness", "Danceability", "Energy", "Instrumentalness", "Liveness", "Speechiness", "Valence"];
   let information = [acoustincness, danceability, energy, instrumentalness, liveness, speechiness, valence];
-
+  
   console.log(information)
-
+  
   let data = [];
-
+  
   let point = {};
   point["Acousticness"] = information[0] * 10;
   point["Danceability"] = information[1] * 10;
@@ -174,106 +187,104 @@ function draw(id) {
   point["Liveness"] = information[4] * 10;
   point["Speechiness"] = information[5] * 10;
   point["Valence"] = information[6] * 10;
-
+  
   data.push(point);
-
-
-  const svg = d3.select("#col1")
-    .append("svg")
-    .attr("width", 650)
-    .attr("height", 650);
-
-
+  
+  
+  
+  
   let radialScale = d3.scaleLinear()
-    .domain([0, 10])
-    .range([0, 250]);
+  .domain([0, 10])
+  .range([0, 250]);
   let ticks = [2, 4, 6, 8, 10];
-
-
-
-
+  
+  
+  
   ticks.forEach(t =>
     svg.append("circle")
-      .attr("cx", 300)
-      .attr("cy", 300)
-      .attr("fill", "none")
-      .attr("stroke", "gray")
-      .attr("r", radialScale(t))
-  );
-
-  ticks.forEach(t =>
-    svg.append("text")
+    .attr("cx", 300)
+    .attr("cy", 300)
+    .attr("fill", "none")
+    .attr("stroke", "gray")
+    .attr("r", radialScale(t))
+    );
+    
+    ticks.forEach(t =>
+      svg.append("text")
       .attr("x", 305)
       .attr("y", 300 - radialScale(t))
       .text((t / 10).toString())
-  );
-
-
-  function angleToCoordinate(angle, value) {
-    let x = Math.cos(angle) * radialScale(value);
-    let y = Math.sin(angle) * radialScale(value);
-    return { "x": 300 + x, "y": 300 - y };
-  }
-
-  for (let i = 0; i < features.length; i++) {
-    let ft_name = features[i];
-    let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
-    let line_coordinate = angleToCoordinate(angle, 10);
-    let label_coordinate = angleToCoordinate(angle, 10.5);
-
-    //draw axis line
-    svg.append("line")
-      .attr("x1", 300)
-      .attr("y1", 300)
-      .attr("x2", line_coordinate.x)
-      .attr("y2", line_coordinate.y)
-      .attr("stroke", "black");
-
-    //draw axis label
-    svg.append("text")
-      .attr("x", label_coordinate.x)
-      .attr("y", label_coordinate.y)
-      .text(ft_name);
-  }
-
-  let line = d3.line()
-    .x(d => d.x)
-    .y(d => d.y);
-  let colors = ["blue"];
-
-
-  function getPathCoordinates(data_point) {
-    let coordinates = [];
-    for (let i = 0; i < features.length; i++) {
-      let ft_name = features[i];
-      let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
-      coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
+      );
+      
+      
+      function angleToCoordinate(angle, value) {
+        let x = Math.cos(angle) * radialScale(value);
+        let y = Math.sin(angle) * radialScale(value);
+        return { "x": 300 + x, "y": 300 - y };
+      }
+      
+      for (let i = 0; i < features.length; i++) {
+        let ft_name = features[i];
+        let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+        let line_coordinate = angleToCoordinate(angle, 10);
+        let label_coordinate = angleToCoordinate(angle, 10.5);
+        
+        //draw axis line
+        svg.append("line")
+        .attr("x1", 300)
+        .attr("y1", 300)
+        .attr("x2", line_coordinate.x)
+        .attr("y2", line_coordinate.y)
+        .attr("stroke", "black");
+        
+        //draw axis label
+        svg.append("text")
+        .attr("x", label_coordinate.x)
+        .attr("y", label_coordinate.y)
+        .text(ft_name);
+      }
+      
+      let line = d3.line()
+      .x(d => d.x)
+      .y(d => d.y);
+      let colors = ["blue"];
+      
+      
+      function getPathCoordinates(data_point) {
+        let coordinates = [];
+        for (let i = 0; i < features.length; i++) {
+          let ft_name = features[i];
+          let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+          coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
+        }
+        return coordinates;
+      }
+      
+      for (let i = 0; i < data.length; i++) {
+        let d = data[i];
+        let color = colors[i];
+        let coordinates = getPathCoordinates(d);
+        
+        //draw the path element
+        svg.append("path")
+        .datum(coordinates)
+        .attr("d", line)
+        .attr("stroke-width", 1)
+        .attr("stroke", color)
+        .attr("fill", color)
+        .attr("stroke-opacity", 1)
+        .attr("opacity", .7);
+      }
+      
     }
-    return coordinates;
-  }
 
-  for (let i = 0; i < data.length; i++) {
-    let d = data[i];
-    let color = colors[i];
-    let coordinates = getPathCoordinates(d);
-
-    //draw the path element
-    svg.append("path")
-      .datum(coordinates)
-      .attr("d", line)
-      .attr("stroke-width", 1)
-      .attr("stroke", color)
-      .attr("fill", color)
-      .attr("stroke-opacity", 1)
-      .attr("opacity", .7);
-  }
-}
-
-
-document.getElementById("button").addEventListener("click", submitClicked);
-
-
-
-// Food 
-// Electric Ave 
-// This World
+    
+    
+    document.getElementById("button").addEventListener("click", submitClicked);
+    
+    
+    
+    // Food 
+    // Electric Ave 
+    // This World
+    
